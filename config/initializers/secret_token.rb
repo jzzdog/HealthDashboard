3cd3d3f58322a50c4c5390b40bb2fea547adf0d1
Dmitriy Anishchenko <dmanishchenko@gmail.com>
2013-10-06 21:10:35 +0400

    My initial commit (jzzdoggy)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HealthDashboard::Application.config.secret_token = 'bb7bce71b97d53d4c74864d16b68262788a58f931483f3f68b4a7252ede03e222244b5df2171387f3568589ad79f008e775881651bcd9c75fa3dbb32b0c4ba8a'
